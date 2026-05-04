fe770d4b86852a032c900f9afd6b3496ca27b89a
Eng-Omar-Hussein <omarhussein2111@gmail.com>
2026-05-05 02:28:59 +0300

    Add CI workflow for Android with Detekt and OWASP Dependency Check

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,27 @@ plugins {
     alias(libs.plugins.android.library) apply false
     id("com.google.dagger.hilt.android") version "2.59.1" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
+    id("org.owasp.dependencycheck") version "12.2.2"
 
+}
+allprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    detekt {
+        config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+        buildUponDefaultConfig = true
+        ignoreFailures = false 
+    }
+}
+
+dependencyCheck {
+    failBuildOnCVSS = 7.0f 
+
+    nvd {
+        apiKey = System.getenv("NVD_API_KEY") 
+    }
+    
+    // Optional: Suppress false positives by generating a suppressions file
+    // suppressionFile = "$rootDir/config/dependency-check-suppressions.xml"
 }
